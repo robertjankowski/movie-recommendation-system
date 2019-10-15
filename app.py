5112b5e2e183851417d1d4c78303795a74425b75
robertjankowski <robjanw11@wp.pl>
2019-10-15 21:28:34 +0200

    add genre recommendation

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import pandas as pd
 from flask import Flask, render_template, request
-from recommendation.recommender import item_based_recom, rename_columns
+from recommendation.recommender import item_based_recom, rename_columns, item_and_genre_based_recom, categories
 
 app = Flask(__name__)
 app.recommender = pd.read_parquet('models/recommender_df.parquet.gzip')
+app.movies = pd.read_parquet('models/movies.parquet.gzip')
 rename_columns(app.recommender)
 
 
@@ -24,8 +25,17 @@ def recommend_movie():
         df = item_based_recom(app.recommender, movie_title).head(10)
     except Exception:
         return render_template('error_title.html')
-    # return render_template('movie.html', tables=[df.to_html(classes='data')], titles=df.columns.values)
     return render_template('movie.html', name='Movie recommendation for {}'.format(movie_title), data=df)
+
+
+@app.route('/genre', methods=['POST'])
+def recommend_movie_genre():
+    movie_title = request.form['movie_title']
+    try:
+        df = item_and_genre_based_recom(item_based_recom(app.recommender, movie_title), app.movies, categories)
+    except Exception:
+        return render_template('error_title.html')
+    return render_template('movie.html', name='Movie and genre recommendation for {}'.format(movie_title), data=df)
 
 
 if __name__ == '__main__':
